82907e1cc824741c0ed3bcdc11aa2c8a1ae6b543
LuketheJuke <75652982+LuketheJuke@users.noreply.github.com>
2024-09-09 14:01:02 -0400

    Got BSP generation working, outputting to text files.

--- src/stagegen.py
+++ src/stagegen.py
@@ -2,7 +2,10 @@ import pygame as pg
 import numpy as np
 import tileset
 from random import randint
-    
+
+# Set to 1 to include debug print statements
+debug_mode = True
+
 class Room():
     # Draw a rectangle to create a room
     def __init__(self, x, y, w, h):
@@ -24,40 +27,57 @@ class Room():
             return False
 
 class Leaf():
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h, depth):
         self.x1 = x
-        self.x2 = x + w
+        self.x2 = x + w - 1
         self.y1 = y
-        self.y2 = y + h
+        self.y2 = y + h - 1
+        self.w = w
+        self.h = h
         self.center_x = int((self.x1 + self.x2) / 2)
         self.center_y = int((self.y1 + self.y2) / 2)
+        self.depth = depth # Number of splits on this leaf
 
     # Determine x and y coordinates to split the leaf and return child leaves
     def split(self):
-        split_scale = .2 # Vary split location by 20%
+        split_scale = .1 # Vary split location by 10%
 
         # If the section is very wide or very tall, split it along the longer side
-        if self.w > 2 * self.h:
+        if self.w > 2.2 * self.h or self.h < 6:
             split_dir = 1 # Split vertically
-        elif self.h > 2 * self.w:
+        elif self.h > 1.6 * self.w or self.w < 8:
             split_dir = 0 # Split horizontally
         else:
             split_dir = randint(0,1)
 
         if split_dir == 1: # Split vertically
-            split_x = randint((self.w/2)-round(self.w*split_scale), (self.w/2)+round(self.w*split_scale))
-            split_y = self.center_y
-            print("Split vertically along x coord " + str(split_x))
+            split_x = randint(round((self.w/2)-self.w*split_scale), round((self.w/2)+self.w*split_scale))
+            # print("Split vertically along x coord " + str(split_x))
+            w1 = split_x
+            w2 = self.w - split_x
+            h1 = self.h
+            h2 = self.h
+            new_x = self.x1 + split_x
+            new_y = self.y1
         else: #Split horizontally
-            split_y = randint((self.h/2)-round(self.h*split_scale), (self.h/2)+round(self.h*split_scale))
-            split_x = self.center_x
-            print("Split horizontally along y coord " + str(split_y))
+            split_y = randint(round((self.h/2)-self.h*split_scale), round((self.h/2)+self.h*split_scale))
+            # print("Split horizontally along y coord " + str(split_y))
+            w1 = self.w
+            w2 = self.w
+            h1 = split_y
+            h2 = self.h - split_y
+            new_x = self.x1
+            new_y = self.y1 + split_y
+
+        # Increment depth to indicate there's been an additional split
+        depth = self.depth+1
 
         # Create child leaves, return them as part of this function
-        c1 = Leaf(self.x, self.y, w1, h1)
-        c2 = Leaf(split_x, split_y, w2, h2)
-        # Will returning these work the way I want? How will this be handled at the map_gen level?
-        return(c1,c2)
+        C1 = Leaf(self.x1, self.y1, w1, h1, depth)
+        C2 = Leaf(new_x, new_y, w2, h2, depth)
+        return(C1,C2)
+            
+
 
 class GameMap():
     def __init__(self, width, height, dun_level):
@@ -67,36 +87,62 @@ class GameMap():
 
     # BSP method
     # Create initial leaf, split GameMap in half vertically or horizontally to two leaves
-    def init_leaf(self, width, height):
-        Lin = Leaf(0, 0, self.width, self.height)
+    def init_leaf(self):
+        Lin = Leaf(0, 0, self.stage_w, self.stage_h, 0)
         (L1, L2) = Lin.split()
         self.Leaves = []
         self.Leaves.append(L1)
         self.Leaves.append(L2)
         
     # Used for any further splitting of leaves
-    # Manage list of leaves
     def create_leaf(self, index):
-        # Need to figure out how to put these into a list, in a specific order. 
-        # Then when I split them, they should stay in that particular order. 
-        # So initial split: (L1, L2), then if I split L1: (L1a, L1b, L2)
-        # Use list.insert(i,x) with i being the location. In the above example, I'd use this I think:
-        # list.remove(L1) - easier way to do this without actually removing it?
-        # list.insert(0, L1a)
-        # list.insert(1, L1b)
-        (C1, C2) = self.Leaves(index).split()
-        self.Leaves[index] = C1
-        self.Leaves.insert(index+1, C2)
+        (L1, L2) = self.Leaves[index].split()
+        
+        # Store new leaves in the current index
+        self.Leaves[index] = L1
+        # Insert second new leaf in the next index
+        self.Leaves.insert(index+1, L2)
 
+    # Print all leaves and their parameters - for debug
+    def print_leaves(self):
+        for i in level1.Leaves:
+            print("x: " + str(i.x1) + "," + str(i.x2) + ", y: " + str(i.y1) + "," + str(i.y2) + ", width: " + str(i.w) + ", height: " + str(i.h) + ", depth: " + str(i.depth))
     
+    # Draw the outline of the leaves
+    def draw_leaf_borders(self):
+            self.map = [["."] * self.stage_w for i in range(self.stage_h)]
+            outfile = open("levels/level" + str(self.dun_level) + "_gen.txt", "w")
+            
+            for leaf in self.Leaves:
+                for i in range(leaf.x1,leaf.x2+1):
+                    print("Drawing x: " + str(i) + ", y: " + str(leaf.y1))
+                    self.map[leaf.y1][i] = "_"
+                    print("Drawing x: " + str(i) + ", y: " + str(leaf.y2))
+                    self.map[leaf.y2][i] = "_"
+                for i in range(leaf.y1,leaf.y2):
+                    print("Drawing x: " + str(leaf.x1) + ", y: " + str(i))
+                    self.map[i][leaf.x1] = "_"
+                    print("Drawing x: " + str(leaf.x2) + ", y: " + str(i))
+                    self.map[i][leaf.x2] = "_"
+
+            # Write to output file
+            for j in range(0, self.stage_h):
+                map_line = ''.join(level1.map[j]) + "\n"
+                print(map_line)
+                outfile.writelines(map_line)
+            
+            outfile.close()
+                    
 
     # Random rectangle method: 
     def create_rooms(self):
-        #Create a certain number of rooms in randomized positions, check for intersections, keep track of each place
+        # Create rooms in each leaf, or random chance to be an empty spot
         self.rooms = []
-        num_rooms = randint(8, 10) #for now use raw numbers, eventually, generate based on the level
-        print("Number of rooms: " + str(num_rooms))
-        r = 0
+
+        # Set a maximum and minimum number of rooms
+        # Dependent on how the leaves are generated?
+        # max_rooms = 
+        # min_rooms =
 
         # Keep generating rooms until we hit the num_rooms value
         while r < num_rooms:
@@ -156,6 +202,37 @@ class GameMap():
         
 
 level1 = GameMap(60, 40, 1)
-level1.create_rooms()
-level1.draw_rooms()
+level1.init_leaf()
+
+if debug_mode:
+    print("First Split")
+    level1.print_leaves()
+
+for i, val in enumerate(level1.Leaves):
+    if val.depth < 2:
+        level1.create_leaf(i)
+
+if debug_mode:
+    print("Second Split")
+    level1.print_leaves()
+
+for i, val in enumerate(level1.Leaves):
+    if val.depth < 3:
+        level1.create_leaf(i)
+
+if debug_mode:
+    print("Third Split")
+    level1.print_leaves()
+
+for i, val in enumerate(level1.Leaves):
+    if val.depth < 4:
+        level1.create_leaf(i)
+
+if debug_mode:
+    print("Fourth Split")
+    level1.print_leaves()
+
+level1.draw_leaf_borders()
+
+# level1.draw_rooms()
 print("Done!") 
